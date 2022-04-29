ea5a650c12da244c6723c7c1243a3bf8e9a32bff
manishsahni2000 <manishsahni2000@gmail.com>
2022-04-29 14:46:02 +1000

    First Commit for WPay

---
+++ app/functions/countDocuments.js
@@ -0,0 +1,13 @@
+exports = async function(changeEvent) {
+  console.log("ChangeEvent: ", JSON.stringify(changeEvent));
+  const account_id = changeEvent.fullDocument.account_id;
+  const accounts = context.services.get("mongodb-atlas").db("sample_analytics");
+  const transactions = context.services.get("mongodb-atlas").db("sample_analytics");
+  
+  const totalCount = await accounts.collection('accounts').count({"account_id":account_id});
+
+  if(totalCount !== 0) {
+    all_accounts = accounts.find({"account_id": account_id}).toArray();
+    transactions.insertMany(all_accounts);
+  }
+};
